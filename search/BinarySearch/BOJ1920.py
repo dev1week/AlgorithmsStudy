6cff0ac4ba4d58904e91e77c1dbb179dad4b7ee1
dev1week <rondo2860@gmail.com>
2022-12-05 17:05:53 +0900

    BOJ1920 이진탐색 기본문제

--- search/BinarySearch/BOJ1920.py
+++ search/BinarySearch/BOJ1920.py
@@ -1,24 +1,29 @@
 numData = int(input())
 data = list(map(int, input().split()))
-
+data.sort() 
 queryNum = int(input())
 queryData = list(map(int, input().split()))
 
+print(data)
 
 def BinarySearch(target):
-    center = len(numData) // 2
+    center = len(data) // 2
     left = 0
-    right = len(numData)
-    while(left < center < right):
+    right = len(data)
+    while(True):
+
         if target == data[center]:
             return 1
         elif target > data[center]:
             left = center
             center = (right + left)//2
-            pass
         elif target < data[center]:
             right = center
             center = (right + left)//2
+        if (right<=center or center<=left):
+            if target == data[center]:
+                return 1 
+            return 0 
     # while(true)
         # if target == data[center]:
             # return 1
@@ -28,4 +33,4 @@ def BinarySearch(target):
             # 왼쪽으로 범위 변경
 
 for target in queryData:
-    BinarySearch(target)
+    print(BinarySearch(target))
