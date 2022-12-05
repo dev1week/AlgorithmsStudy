cf088203f235d6fd43f73a468f154493c2ae9368
dev1week <rondo2860@gmail.com>
2022-12-05 15:26:10 +0900

    BOJ1167.py 그래프에서 가장 먼거리 찾기는 BFS와 distance 배열을 사용하여 푼다

--- search/BOJ1167.py
+++ search/BOJ1167.py
@@ -1,20 +1,4 @@
-from collections import deque
-nodeNum = int(input())
 
-adjcList = [[] for _ in range(nodeNum+1)] 
-visited =[False for _ in range(nodeNum+1)]
-distance = [0 for _ in range(nodeNum+1)]
-
-for _ in range(nodeNum):
-    data = deque(map(int, input().split()))
-    node = data.popleft() 
-    while data:
-        if data[0] == -1:
-            break 
-        adjcNode = data.popleft() 
-        distance = data.popleft() 
-        adjcList[node].append((adjcNode, distance))
-    print(adjcList)
 
 
 # 트리의 지름은 노드 중 두 노드 사이의 거리가 가장 긴 것 
@@ -31,21 +15,39 @@ for _ in range(nodeNum):
                 # 큐에 넣기
                 # distance[nodeInfo[1]] =distance[befoNode] +nodeInfo[2] 
                 # befoNode = nodeInfo[0]
+from collections import deque
+nodeNum = int(input())
+
+adjcList = [[] for _ in range(nodeNum+1)]
+visited =[False for _ in range(nodeNum+1)]
+distance = [0 for _ in range(nodeNum+1)]
+
+for _ in range(nodeNum):
+    data = deque(map(int, input().split()))
+    node = data.popleft()
+    while data:
+        if data[0] == -1:
+            break
+        adjcNode = data.popleft()
+        weight = data.popleft()
+        adjcList[node].append([adjcNode, weight])
+
 
 def BFS(start):
-    queue = deque() 
+    queue = deque()
     if not visited[start]:
         visited[start] = True 
         queue.append(start)
-        beforeNode = start 
     while queue:
-        current = queue.popleft() 
+        current = queue.popleft()
         for nodeInfo in adjcList[current]:
-            if not visited[nodeInfo[0]]:
-                visited[nodeInfo[0]] = True 
-                queue.append(nodeInfo[0])
-                distance[nodeInfo[1]] = distance[beforeNode] + nodeInfo[2]
-                beforeNode = nodeInfo[0]
+            nextNode = int(nodeInfo[0])
+            nodeDistance = int(nodeInfo[1])
+            if not visited[nextNode]:
+                visited[nextNode] = True
+                queue.append(nextNode)
+                distance[nextNode]=distance[current] + nodeDistance
+
 
 BFS(1)
 nextTarget = distance.index(max(distance))
