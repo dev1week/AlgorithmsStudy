7f0b969cfbc733cb1f8a3a45ad35a2081c137a9d
dev1week <rondo2860@gmail.com>
2022-12-14 16:01:51 +0900

    [삼성기출 하반기 오전] : 필요한 클래스 구현

---
+++ Samsung/Dice.java
@@ -0,0 +1,15 @@
+package Samsung;
+
+public class Dice {
+    int x;
+    int y; 
+    int num[] = new int[6];
+    int dir;
+    private int get_bottom(){
+        return 0; 
+    }
+    private void move(){
+
+    }
+
+}
---
+++ Samsung/Morning.java
@@ -0,0 +1,9 @@
+package Samsung; 
+
+public class Morning{
+    
+    public static void main(String[] args){
+        final int  NM = 20;
+        System.out.println(NM);
+    }
+}
