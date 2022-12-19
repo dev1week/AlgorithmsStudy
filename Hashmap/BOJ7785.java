f57e5e1d35c5c5bd419f15b9115152c927885183
dev1week <rondo2860@gmail.com>
2022-12-19 22:52:17 +0900

    [7785] 기본 로직 설계

---
+++ Hashmap/BOJ7785.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.Iterator;
+
+public class test {
+    static final int S = 5;
+    public static void main(String[] args)throws IOException{
+//제한사항
+//1. 
+        //로직
+        //입력받기
+        //입력 받은 횟수에 대해 
+            //첫번째값 입력 
+            //두번째값 입력
+            //두번째값 == enter
+                // hashmap에 넣기
+            //두번째값 == leave
+                // hashmap에서 찾아 빼기 
+    }
+
+}
+
