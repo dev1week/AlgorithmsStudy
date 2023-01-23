2b308007362aa76fe4e279d108325925320dc46d
dev1week <rondo2860@gmail.com>
2023-01-23 22:43:31 +0900

    해쉬맵 풀기, 정렬하는 방법 익히기

--- Hashmap/BOJ13414.java
+++ Hashmap/BOJ13414.java
@@ -2,44 +2,43 @@ import java.io.*;
 import java.util.*; 
 
 public class BOJ13414 {
-    static int enableNum;// 100,000
-    static int listNum; //500,000
+    static HashMap<String, Integer> enroll = new HashMap<>();
     static StringBuilder sb = new StringBuilder(); 
-    static LinkedHashSet<String> que = new LinkedHashSet<>();
+    static int enableNum;
+    static int logNum; 
+    static LinkedList<Map.Entry<String, Integer>> sortedList;
+    
 
-    //자료구조에 입력을 받는다. 
-    //중복일 경우 삭제한다. 
-    //맨앞에서 정해진 숫자만큼만 출력한다. 
     static void init() throws IOException{
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer tokens = new StringTokenizer(buffer.readLine());
         enableNum = Integer.valueOf(tokens.nextToken());
-        listNum = Integer.valueOf(tokens.nextToken());
-        for(int num=0; num<listNum; num++){
-            String code = buffer.readLine();
-            if(que.contains(code)){
-                que.remove(code);
-                que.add(code);
-            }else{
-                que.add(code);
+        logNum = Integer.valueOf(tokens.nextToken());
+        for(int i=0; i<logNum; i++){
+            String data = buffer.readLine(); 
+            if(enroll.containsKey(data)){
+                enroll.replace(data, i);
+            }
+            else{
+                enroll.put(data, i);
             }
         }
+        sortedList = new LinkedList<>(enroll.entrySet());
+        sortedList.sort(Map.Entry.comparingByValue());
     }
-
-    static void print(){
-        Iterator<String> it = que.iterator();
-        for(int i=0; i< enableNum; i++){
-            sb.append(it.next()+"\n");
-        }
-    }
-    
     public static void main(String[] args) throws IOException{
         init(); 
-        print();
-        System.out.println(sb);
-        //입력받기 
+        Iterator it = sortedList.iterator();
 
-        //출력하기 
-        
+        if(enableNum >= sortedList.size() ){
+            enableNum = sortedList.size(); 
+        }
+
+        for(int i=0; i<enableNum; i++){
+            StringTokenizer result = new StringTokenizer(it.next().toString());
+            sb.append(result.nextToken("=")+"\n");
+        }
+        System.out.println(sb);
     }
+
 }
