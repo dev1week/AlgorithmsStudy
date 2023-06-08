c3828ba5b19495f145ce3be81c4a35ef8bf73e04
dev1week <rondo2860@gmail.com>
2023-06-08 23:44:00 +0900

    #7 - BOJ1907 (풀이중)

---
+++ solved ac/String/BOJ1907.java
@@ -0,0 +1,30 @@
+package String;
+
+import java.util.*; 
+public class BOJ1907 {
+	
+	static Scanner sc = new Scanner(System.in); 
+	
+	static HashMap<Character, Integer> left = new HashMap<>(); 
+	static HashMap<Character, Integer> right = new HashMap<>();
+	
+	static String line; 
+	
+	static void init() {
+		line = sc.next(); 
+	}
+	
+	static void main(String[] args) {
+		//line 순
+			//+ 스킵 
+			//문자인가?
+				//다음 단어가 있는가? 
+					//다음 단어 문자인가?
+						//단어만 hashset에 넣어준다. 
+					//다음 단어가 숫자인가?
+						//숫자까지 같이 본
+				//다음 단어가 없는가? 
+				
+			 
+	}
+}
